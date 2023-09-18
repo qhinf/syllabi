8fb6b7d81b9a06e3852486d4f29651305490a9e4
Arthur Rump <hello@arthurrump.com>
2023-09-18 18:04:44 +0200

    Voeg simpele index met alle syllabi toe

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 import os
+import re
 import shutil
 import subprocess
 import sys
 import yaml
 
 from git import Repo
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from mergedeep import merge
 from os import path
 
@@ -16,15 +18,22 @@ def get_repo_path(module: str | None = None, version: str | None = None) -> str:
         return path.join(syllabi_folder, module)
     else:
         return path.join(syllabi_folder, module, version)
+    
+year_block_pattern = re.compile(r"(\d{2})(\d{2})-(\d)")
 
 # Create directory for the HTML build files
-os.makedirs("_build", exist_ok = True)
+shutil.rmtree("_build")
+shutil.copytree("static", "_build")
 
 # Read the base configuration file into a Python dictionary
 with open("_config_base.yml", mode = "r") as config_base_file:
     config_base = yaml.safe_load(config_base_file)
 
+modules = []
 for module in os.listdir(get_repo_path()):
+    module_title = module
+    versions = []
+
     for version in os.listdir(get_repo_path(module)):
         repo_path = get_repo_path(module, version)
         jb_path = path.join(repo_path, "syllabus")
@@ -64,3 +73,29 @@ for module in os.listdir(get_repo_path()):
             shutil.copytree(path.join(jb_path, "_build", "html"), path.join("_build", module, version))
 
         os.remove(path.join(jb_path, "_config_ext.yml"))
+
+        if (match := year_block_pattern.match(version)) is not None:
+            version_title = f"{match.group(1)}/{match.group(2)} - Blok {match.group(3)}"
+        else:
+            version_title = version
+
+        module_title = new_ref_config["title"]
+        versions.append({ "slug": version, "title": version_title })
+
+    versions.reverse()
+    modules.append({ "slug": module, "title": module_title, "versions": versions })
+
+modules.sort(key = lambda module: module["title"])
+
+jinja_env = Environment(loader = FileSystemLoader("templates"), autoescape = select_autoescape())
+
+index_template = jinja_env.get_template("index.html")
+print(f"[index] Writing index.html")
+with open(path.join("_build", "index.html"), mode = "w") as index_file:
+    index_file.write(index_template.render(modules = modules))
+
+module_index_template = jinja_env.get_template("module_index.html")
+for module in modules:
+    print(f"[{module['slug']}] Writing index.html")
+    with open(path.join("_build", module["slug"], "index.html"), mode = "w") as module_index_file:
+        module_index_file.write(module_index_template.render(module = module))
